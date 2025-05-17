494197f395702e3eb48a94f8abec6d59110619e9
phoekerson <mintoumbacaleb@gmail.com>
2025-05-17 02:57:59 +0200

    first test

--- pool/backend/test/Pool.t.sol
+++ pool/backend/test/Pool.t.sol
@@ -5,12 +5,28 @@ import "forge-std/Test.sol";
 import { Pool } from "../src/Pool.sol";
 
 contract PoolTest is Test{
-    address owner = maleAddr("User0");
+    address owner = makeAddr("User0");
     address addr1 = makeAddr("User1");
     address addr2 = makeAddr("User2");
     address addr3 = makeAddr("User3");
 
-    uint256 end = 4 weeks; 
+    uint256 duration = 4 weeks; 
     uint256 goal = 10 ether;
 
+    Pool pool;
+
+    function setUp() public{
+        vm.prank(owner);
+        pool = new Pool(duration, goal);
+    }
+
+    function test_ContractDeployedSuccessfully()
+    public {
+        address _owner = pool.owner();
+        assertEq(owner, _owner);
+        uint256 _end = pool.end();
+        assertEq(block.timestamp + duration, _end);
+        uint256 _goal = pool.goal();
+        assertEq(goal, _goal);
+    }
 }
